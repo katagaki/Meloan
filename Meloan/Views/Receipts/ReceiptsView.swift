223f01dcf32bf502ed606f017e8111368f478a47
katagaki <68231035+katagaki@users.noreply.github.com>
2023-09-17 12:37:05 +0800

    Localize sample data

--- Meloan/Views/Receipts/ReceiptsView.swift
+++ Meloan/Views/Receipts/ReceiptsView.swift
@@ -39,28 +39,7 @@ struct ReceiptsView: View {
                 ToolbarItem(placement: .topBarTrailing) {
                     HStack {
                         Button {
-                            // Add sample data
-                            // swiftlint:disable line_length
-                            let person1 = Person(name: "Akagi", photo: UIImage(named: "Akagi")!.pngData())
-                            let person2 = Person(name: "Muramoto", photo: UIImage(named: "Muramoto")!.pngData())
-                            let person3 = Person(name: "Kuroko", photo: UIImage(named: "Kuroko")!.pngData())
-                            let receiptItem1 = ReceiptItem(name: "Steak Platter", price: 42.00, amount: 1)
-                            let receiptItem2 = ReceiptItem(name: "Spaghetti Carbonara (Extra Sauce, Extra Cheese)", price: 12.00, amount: 1, purchasedBy: person1)
-                            let receiptItem3 = ReceiptItem(name: "Fish & Chips", price: 11.00, amount: 1, purchasedBy: person2)
-                            let receiptItem4 = ReceiptItem(name: "Sirloin Steak (Black Pepper Sauce)", price: 16.00, amount: 1, purchasedBy: person3)
-                            let receiptItem5 = ReceiptItem(name: "Ice Water", price: 1.00, amount: 3)
-                            let discountItem = DiscountItem(name: "Card Discount", price: 6.00)
-                            let taxItem1 = TaxItem(name: "Service Charge", price: 8.10)
-                            let taxItem2 = TaxItem(name: "GST", price: 6.48)
-                            let receipt = Receipt(name: "Team Dinner at Johnny's Diner")
-                            // swiftlint:enable line_length
-                            receipt.addReceiptItems(from: [receiptItem1, receiptItem2, receiptItem3,
-                                                           receiptItem4, receiptItem5])
-                            receipt.addDiscountItems(from: [discountItem])
-                            receipt.addTaxItems(from: [taxItem1, taxItem2])
-                            receipt.setPersonWhoPaid(to: person1)
-                            modelContext.insert(receipt)
-                            try? modelContext.save()
+                            createSampleData()
                         } label: {
                             Text("Shared.CreateSampleData")
                         }
@@ -80,5 +59,37 @@ struct ReceiptsView: View {
             }
             .navigationTitle("ViewTitle.Receipts")
         }
+    }
+
+    func createSampleData() {
+        let person1 = Person(name: NSLocalizedString("SampleData.Akagi", comment: ""),
+                             photo: UIImage(named: "Akagi")!.pngData())
+        let person2 = Person(name: NSLocalizedString("SampleData.Muramoto", comment: ""),
+                             photo: UIImage(named: "Muramoto")!.pngData())
+        let person3 = Person(name: NSLocalizedString("SampleData.Kuroko", comment: ""),
+                             photo: UIImage(named: "Kuroko")!.pngData())
+        let receiptItem1 = ReceiptItem(name: NSLocalizedString("SampleData.Item1", comment: ""),
+                                       price: 42.00, amount: 1)
+        let receiptItem2 = ReceiptItem(name: NSLocalizedString("SampleData.Item2", comment: ""),
+                                       price: 12.00, amount: 1, purchasedBy: person1)
+        let receiptItem3 = ReceiptItem(name: NSLocalizedString("SampleData.Item3", comment: ""),
+                                       price: 11.00, amount: 1, purchasedBy: person2)
+        let receiptItem4 = ReceiptItem(name: NSLocalizedString("SampleData.Item4", comment: ""),
+                                       price: 16.00, amount: 1, purchasedBy: person3)
+        let receiptItem5 = ReceiptItem(name: NSLocalizedString("SampleData.Item5", comment: ""),
+                                       price: 1.00, amount: 3)
+        let discountItem = DiscountItem(name: NSLocalizedString("SampleData.Discount", comment: ""),
+                                        price: 6.00)
+        let taxItem1 = TaxItem(name: NSLocalizedString("SampleData.TaxItem1", comment: ""),
+                               price: 8.10)
+        let taxItem2 = TaxItem(name: NSLocalizedString("SampleData.TaxItem2", comment: ""),
+                               price: 6.48)
+        let receipt = Receipt(name: NSLocalizedString("SampleData.ReceiptName", comment: ""))
+        receipt.addReceiptItems(from: [receiptItem1, receiptItem2, receiptItem3,
+                                       receiptItem4, receiptItem5])
+        receipt.addDiscountItems(from: [discountItem])
+        receipt.addTaxItems(from: [taxItem1, taxItem2])
+        receipt.setPersonWhoPaid(to: person1)
+        modelContext.insert(receipt)
     }
 }
