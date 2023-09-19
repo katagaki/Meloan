6a2a1d126b3761796c7630e1180e110f91bbe580
katagaki <68231035+katagaki@users.noreply.github.com>
2023-09-20 00:55:46 +0800

    Clean up files

--- Assistant/Entities/ReceiptEntity.swift
+++ Assistant/Entities/ReceiptEntity.swift
@@ -11,6 +11,18 @@ import SwiftData
 
 struct ReceiptEntity: AppEntity {
 
+    static var sharedModelContainer: ModelContainer = {
+          let schema = Schema([
+              Receipt.self, Person.self, ReceiptItem.self, DiscountItem.self, TaxItem.self
+          ])
+          let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+          do {
+              return try ModelContainer(for: schema, configurations: [modelConfiguration])
+          } catch {
+              fatalError("Could not create ModelContainer: \(error)")
+          }
+      }()
+
     typealias DefaultQuery = ReceiptQuery
 
     let id: String
@@ -22,61 +34,36 @@ struct ReceiptEntity: AppEntity {
     var displayRepresentation: DisplayRepresentation {
         DisplayRepresentation(title: "\(name)")
     }
+
+    @MainActor
+    static func allReceipts() -> [Receipt] {
+        if let allReceipts = try? sharedModelContainer.mainContext.fetch(FetchDescriptor<Receipt>()) {
+            return allReceipts.sorted(by: { $0.name < $1.name })
+        } else {
+            return []
+        }
+    }
 }
 
 struct ReceiptQuery: EntityQuery {
 
     typealias Entity = ReceiptEntity
 
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Receipt.self, Person.self, ReceiptItem.self, DiscountItem.self, TaxItem.self
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+    func entities(for identifiers: [ReceiptEntity.ID]) async throws -> [ReceiptEntity] {
+         await ReceiptEntity.allReceipts()
+             .map({ receipt in
+             ReceiptEntity(id: receipt.id, name: receipt.name)
+         })
+             .filter { identifiers.contains($0.id) }
+     }
 
-    func entities(for identifiers: [Entity.ID]) async throws -> [Entity] {
-        let allReceipts = await allReceipts()
-            .map({ receipt in
-                ReceiptEntity(id: receipt.id, name: receipt.name)
-            })
-            .filter { identifiers.contains($0.id) }
-        for receipt in allReceipts {
-            debugPrint(#function + " \(receipt.id): \(receipt.name)")
-        }
-        return allReceipts
-    }
+     func suggestedEntities() async throws -> [ReceiptEntity] {
+         await ReceiptEntity.allReceipts().map({ receipt in
+             ReceiptEntity(id: receipt.id, name: receipt.name)
+         })
+     }
 
-    func suggestedEntities() async throws -> [Entity] {
-        let allReceipts = await allReceipts()
-            .map({ receipt in
-                ReceiptEntity(id: receipt.id, name: receipt.name)
-            })
-        for receipt in allReceipts {
-            debugPrint(#function + " \(receipt.id): \(receipt.name)")
-        }
-        return allReceipts
-    }
-
-    func defaultResult() async -> Entity? {
-        return try? await suggestedEntities().first
-    }
-
-    func allReceipts() async -> [Receipt] {
-        await MainActor.run {
-            do {
-                let allReceipts = try sharedModelContainer
-                    .mainContext.fetch(FetchDescriptor<Receipt>())
-                return allReceipts.sorted(by: { $0.name < $1.name })
-            } catch {
-                debugPrint(error.localizedDescription)
-            }
-            return []
-        }
-    }
+     func defaultResult() async -> ReceiptEntity? {
+         try? await suggestedEntities().first
+     }
 }
