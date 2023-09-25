f4b335578ffd2eda0dd6e6133bfbacdb2d7d9594
katagaki <68231035+katagaki@users.noreply.github.com>
2023-09-25 22:10:28 +0800

    Handle wrapping and page breaks correctly in PDF export

--- Meloan/Views/Shared/PDFExporterView.swift
+++ Meloan/Views/Shared/PDFExporterView.swift
@@ -61,97 +61,122 @@ struct PDFExporterView: View {
         let pdfData = renderer.pdfData { context in
             context.beginPage()
             // Dynamic variables
-            let minX: Double = 10.0
+            var margin: Double = 10.0
             var minY: Double = 10.0
             // Draw title
             let titleSize = drawTitle(receipt.name,
-                                      x: minX, y: minY)
+                                      x: margin, y: minY)
             // Draw logo
             drawImage(UIImage(named: "PDF.Watermark")!, x: 597.6 - 10.0 - titleSize.height, y: 10.0,
                       width: titleSize.height, height: titleSize.height)
             minY += titleSize.height + 10.0
-            drawDivider(x: minX, y: minY)
+            drawDivider(x: margin, y: minY)
             minY += 1.0 + 20.0
             // Draw receipt items
             if !receipt.items().isEmpty {
                 let purchasedItemsSubtitleSize = drawSubtitle(NSLocalizedString("Receipt.PurchasedItems", comment: ""),
-                                                              x: minX, y: minY)
+                                                              x: margin, y: minY)
                 minY += purchasedItemsSubtitleSize.height + 10.0
                 for item in receipt.items() {
-                    let itemNameSize = drawLeadingText(item.name, x: minX, y: minY)
-                    let priceSize = drawPriceText(item.price, x: minX, y: minY)
-                    if item.paid {
-                        drawHorizontalLine(x: minX, y: minY + itemNameSize.height / 2, length: itemNameSize.width)
-                        drawHorizontalLine(x: 587.6 - priceSize.width, y: minY + priceSize.height / 2,
-                                           length: priceSize.width)
-                    }
-                    minY += max(itemNameSize.height, priceSize.height) + 10.0
-                    if let person = item.person {
-                        let personNameSize = drawLeadingSecondaryText(person.name, x: minX + 26.0, y: minY)
-                        if let photo = person.photo {
-                            drawImage(UIImage(data: photo)!, x: minX, y: minY + (personNameSize.height / 2) - 8.0)
-                        } else {
-                            drawImage(UIImage(named: "Profile.Generic.Circle")!,
-                                      x: minX, y: minY + (personNameSize.height / 2) - 8.0)
-                        }
-                        minY += personNameSize.height + 10.0
-                    } else {
-                        let sharedNameSize = drawLeadingSecondaryText(NSLocalizedString("Shared.Shared", comment: ""),
-                                                                      x: minX + 26.0, y: minY)
-                        drawImage(UIImage(named: "Profile.Shared.Circle")!,
-                                  x: minX, y: minY + (sharedNameSize.height / 2) - 8.0)
-                        minY += sharedNameSize.height + 10.0
-                    }
+                    startNewPageIfNextItemWillOverflowPage(context, estimatedHeight: 52.0, minY: &minY)
+                    drawReceiptItem(context, item: item, minX: &margin, minY: &minY)
                 }
                 minY += 10.0
-                drawDivider(x: minX, y: minY)
+                drawDivider(x: margin, y: minY)
                 minY += 20.0
             }
             // Draw discount items
             if !receipt.discountItems().isEmpty {
                 let discountItemsSubtitleSize = drawSubtitle(NSLocalizedString("Receipt.Discounts", comment: ""),
-                                                             x: minX, y: minY)
+                                                             x: margin, y: minY)
                 minY += discountItemsSubtitleSize.height + 10.0
                 for item in receipt.discountItems() {
-                    let itemNameSize = drawLeadingText(item.name, x: minX, y: minY)
-                    let priceSize = drawPriceText(item.price, x: minX, y: minY)
-                    minY += max(itemNameSize.height, priceSize.height) + 10.0
+                    startNewPageIfNextItemWillOverflowPage(context, estimatedHeight: 36.0, minY: &minY)
+                    drawDiscountItem(context, item: item, minX: &margin, minY: &minY)
                 }
                 minY += 10.0
-                drawDivider(x: minX, y: minY)
+                drawDivider(x: margin, y: minY)
                 minY += 1.0 + 20.0
             }
             // Draw tax items
             if !receipt.taxItems().isEmpty {
                 let taxItemsSubtitleSize = drawSubtitle(NSLocalizedString("Receipt.Taxes", comment: ""),
-                                                        x: minX, y: minY)
+                                                        x: margin, y: minY)
                 minY += taxItemsSubtitleSize.height + 10.0
                 for item in receipt.taxItems() {
-                    let itemNameSize = drawLeadingText(item.name, x: minX, y: minY)
-                    let priceSize = drawPriceText(item.price, x: minX, y: minY)
-                    minY += max(itemNameSize.height, priceSize.height) + 10.0
+                    startNewPageIfNextItemWillOverflowPage(context, estimatedHeight: 36.0, minY: &minY)
+                    drawTaxItem(context, item: item, minX: &margin, minY: &minY)
                 }
                 minY += 10.0
-                drawDivider(x: minX, y: minY)
+                drawDivider(x: margin, y: minY)
                 minY += 20.0
             }
             // Draw sum before and after tax
+            startNewPageIfNextItemWillOverflowPage(context, estimatedHeight: 36.0, minY: &minY)
+            let totalBeforeTaxPriceSize = drawPriceText(receipt.sumOfItems(), x: margin, y: minY)
             let totalBeforeTaxSize = drawLeadingText(NSLocalizedString("Receipt.Total.BeforeTax", comment: ""),
-                                                     x: minX, y: minY)
-            let totalBeforeTaxPriceSize = drawPriceText(receipt.sumOfItems(), x: minX, y: minY)
+                                                     x: margin, y: minY, maxWidth: 567.6 - totalBeforeTaxPriceSize.width)
             minY += max(totalBeforeTaxSize.height, totalBeforeTaxPriceSize.height) + 10.0
-            let totalAfterTaxSize = drawLeadingText(NSLocalizedString("Receipt.Total.AfterTax", comment: ""),
-                                                    font: .boldSystemFont(ofSize: 16.0),
-                                                    x: minX, y: minY)
             let totalAfterTaxPriceSize = drawPriceText(receipt.sum(),
                                                        font: .monospacedSystemFont(ofSize: 16.0, weight: .bold),
-                                                       x: minX, y: minY)
+                                                       x: margin, y: minY)
+            let totalAfterTaxSize = drawLeadingText(NSLocalizedString("Receipt.Total.AfterTax", comment: ""),
+                                                    font: .boldSystemFont(ofSize: 16.0),
+                                                    x: margin, y: minY, maxWidth: 567.6 - totalAfterTaxPriceSize.width)
             minY += max(totalAfterTaxSize.height, totalAfterTaxPriceSize.height) + 20.0
-            drawDivider(x: minX, y: minY)
+            drawDivider(x: margin, y: minY)
         }
         return pdfData
     }
     // swiftlint:enable function_body_length
+
+    func startNewPageIfNextItemWillOverflowPage(_ context: UIGraphicsPDFRendererContext, estimatedHeight: Double, minY: inout Double) {
+        if minY + estimatedHeight > 842.4 {
+            context.beginPage()
+            minY = 10.0
+        }
+    }
+    func drawReceiptItem(_ context: UIGraphicsPDFRendererContext, item: ReceiptItem,
+                         minX: inout Double, minY: inout Double) {
+        let priceSize = drawPriceText(item.price, x: minX, y: minY)
+        let itemNameSize = drawLeadingText(item.name, x: minX, y: minY, maxWidth: 567.6 - priceSize.width)
+        if item.paid {
+            drawHorizontalLine(x: minX, y: minY + itemNameSize.height / 2, length: itemNameSize.width)
+            drawHorizontalLine(x: 587.6 - priceSize.width, y: minY + priceSize.height / 2,
+                               length: priceSize.width)
+        }
+        minY += max(itemNameSize.height, priceSize.height) + 10.0
+        if let person = item.person {
+            let personNameSize = drawLeadingSecondaryText(person.name, x: minX + 26.0, y: minY)
+            if let photo = person.photo {
+                drawImage(UIImage(data: photo)!, x: minX, y: minY + (personNameSize.height / 2) - 8.0)
+            } else {
+                drawImage(UIImage(named: "Profile.Generic.Circle")!,
+                          x: minX, y: minY + (personNameSize.height / 2) - 8.0)
+            }
+            minY += personNameSize.height + 10.0
+        } else {
+            let sharedNameSize = drawLeadingSecondaryText(NSLocalizedString("Shared.Shared", comment: ""),
+                                                          x: minX + 26.0, y: minY)
+            drawImage(UIImage(named: "Profile.Shared.Circle")!,
+                      x: minX, y: minY + (sharedNameSize.height / 2) - 8.0)
+            minY += sharedNameSize.height + 10.0
+        }
+    }
+
+    func drawDiscountItem(_ context: UIGraphicsPDFRendererContext, item: DiscountItem,
+                          minX: inout Double, minY: inout Double) {
+        let priceSize = drawPriceText(item.price, x: minX, y: minY)
+        let itemNameSize = drawLeadingText(item.name, x: minX, y: minY, maxWidth: 567.6 - priceSize.width)
+        minY += max(itemNameSize.height, priceSize.height) + 10.0
+    }
+
+    func drawTaxItem(_ context: UIGraphicsPDFRendererContext, item: TaxItem,
+                     minX: inout Double, minY: inout Double) {
+        let priceSize = drawPriceText(item.price, x: minX, y: minY)
+        let itemNameSize = drawLeadingText(item.name, x: minX, y: minY, maxWidth: 567.6 - priceSize.width)
+        minY += max(itemNameSize.height, priceSize.height) + 10.0
+    }
 
     // swiftlint:disable identifier_name
     func drawTitle(_ text: String,
@@ -159,7 +184,7 @@ struct PDFExporterView: View {
         let largeFontAttributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 36.0)]
         let title = NSAttributedString(string: text, attributes: largeFontAttributes)
         let titleSize = title.size()
-        let titleRect = CGRect(x: x, y: y, width: titleSize.width, height: titleSize.height)
+        let titleRect = CGRect(x: x, y: y, width: 577.6 - titleSize.height, height: titleSize.height)
         title.draw(in: titleRect)
         return titleSize
     }
@@ -175,11 +200,11 @@ struct PDFExporterView: View {
     }
 
     func drawLeadingText(_ text: String, font: UIFont = .systemFont(ofSize: 16.0),
-                         x: Double, y: Double) -> CGSize {
+                         x: Double, y: Double, maxWidth: Double) -> CGSize {
         let regularFontAttributes = [NSAttributedString.Key.font: font]
         let text = NSAttributedString(string: text, attributes: regularFontAttributes)
         let textSize = text.size()
-        let textRect = CGRect(x: x, y: y, width: textSize.width, height: textSize.height)
+        let textRect = CGRect(x: x, y: y, width: maxWidth, height: textSize.height)
         text.draw(in: textRect)
         return textSize
     }
